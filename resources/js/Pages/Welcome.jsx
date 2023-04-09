1b89ce343205cf22eaae5b6c1f16ea7cff2864eb
yuki fujimoto <tenriyu0416@gmail.com>
2023-04-09 14:52:33 +0900

    first

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -189,7 +189,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                 </svg>
                             </a>
                             <Link href="https://tenriyunewcomer.com">My BBSへ</Link>
-
+                            <a href="https://tenriyunewcomer.com">My BBSへ</a>
                             <div className="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                                 <div>
                                     <div className="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
